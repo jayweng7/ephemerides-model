56298ef6fe67b6653341b6f4ce7d5a5b6b649772
Jay Weng <jay.weng7@gmail.com>
2017-06-20 18:03:22 -0400

    added all 9 planets

--- ephemerides_model.py
+++ ephemerides_model.py
@@ -24,7 +24,7 @@ class planet():
         while (self.start not in s):
             s = file.readline()
             
-        ephemeris = np.empty([366, 3])
+        ephemeris = np.empty([4750, 3])
         i = 0
         
         while True:
@@ -64,9 +64,29 @@ class planet():
 #      ephemeris[i][2] = s5
 #      i += 1
 #==============================================================================
+#ephemerides = []
 
+planets = []
+
+mercury = planet
+venus = planet
 earth = planet
-ephemeris = earth.getData(earth, 'earth.txt')
+mars = planet
+jupiter = planet
+saturn = planet
+uranus = planet
+neptune = planet
+pluto = planet
+
+ephemeris_m1 = mercury.getData(mercury, 'mercury.txt')
+ephemeris_v = venus.getData(venus, 'venus.txt')
+ephemeris_e = earth.getData(earth, 'earth.txt')
+ephemeris_m = mars.getData(mars, 'mars.txt')
+ephemeris_j = jupiter.getData(jupiter, 'jupiter.txt')
+ephemeris_s = saturn.getData(saturn, 'saturn.txt')
+ephemeris_u = uranus.getData(uranus, 'uranus.txt')
+ephemeris_n = neptune.getData(neptune, 'neptune.txt')
+ephemeris_p = pluto.getData(pluto, 'pluto.txt')
 
 minmax = 1000000000
 
@@ -91,12 +111,20 @@ def func(num):
 
     ax.set_xbound(-minmax, minmax)
     ax.set_ybound(-minmax, minmax)
-    ax.set_zbound(-minmax/100, minmax/100)
+    ax.set_zbound(-minmax/10, minmax/10)
 
     ax.scatter3D(0,0,0,zdir='z')
-    ax.scatter3D(ephemeris[num][0], ephemeris[num][1], ephemeris[num][2])  
+    ax.scatter3D(ephemeris_m1[num][0], ephemeris_m1[num][1], ephemeris_m1[num][2])
+    ax.scatter3D(ephemeris_v[num][0], ephemeris_v[num][1], ephemeris_v[num][2])
+    ax.scatter3D(ephemeris_e[num][0], ephemeris_e[num][1], ephemeris_e[num][2])  
+    ax.scatter3D(ephemeris_m[num][0], ephemeris_m[num][1], ephemeris_m[num][2])
+    ax.scatter3D(ephemeris_j[num][0], ephemeris_j[num][1], ephemeris_j[num][2])
+    ax.scatter3D(ephemeris_s[num][0], ephemeris_s[num][1], ephemeris_s[num][2])
+    ax.scatter3D(ephemeris_u[num][0], ephemeris_u[num][1], ephemeris_u[num][2])
+    ax.scatter3D(ephemeris_n[num][0], ephemeris_n[num][1], ephemeris_n[num][2])
+    ax.scatter3D(ephemeris_p[num][0], ephemeris_p[num][1], ephemeris_p[num][2])
 
-anim = animation.FuncAnimation(fig, func, 366, interval=1, blit=False)
+anim = animation.FuncAnimation(fig, func, 4750, interval=1, blit=False)
 
 
 plt.show()
